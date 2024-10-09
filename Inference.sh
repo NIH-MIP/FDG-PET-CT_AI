fd77320fef7888ad2317a68edc103960eb5dbbfc
F_Haque_AIR <126179790+HFahmida@users.noreply.github.com>
2024-10-09 13:39:52 -0400

    Update Inference.sh

--- Inference.sh
+++ Inference.sh
@@ -2,4 +2,4 @@ export nnUNet_raw="path-to-folder/nnUNet_raw"
 export nnUNet_preprocessed="path-to-folder/nnUNet_preprocessed"
 export nnUNet_results="path-to-folder/nnUNet_results"
 
-nnUNetv2_predict -d DATAID -i $nnUNet_raw/DATAID/imagesTe/ -o $nnUNet_results/DATAID/nnUNetTrainer__nnUNetPlans__3d_fullres/Test_Set/ -f  0 1 2 3 4 -tr nnUNetTrainer -c 3d_fullres -p nnUNetPlans
+nnUNetv2_predict -d Dataset101_PETCT -i $nnUNet_raw/Dataset101_PETCT/imagesTe/ -o $nnUNet_results/Dataset101_PETCT/nnUNetTrainer__nnUNetPlans__3d_fullres/Test_Set/ -f  0 1 2 3 4 -tr nnUNetTrainer -c 3d_fullres -p nnUNetPlans
